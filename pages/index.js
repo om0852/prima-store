0eb95b07c17bc303fee001d3dcdd88056615ffaf
om salunke <salunkeom474@gmail.com>
2024-07-31 10:02:05 -0700

    sidebar implemented

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Inter } from "next/font/google";
+import Nav from "./components/Nav";
+import Layout from "./components/Layout";
+import { useState } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  const { data: session } = useSession();
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>Hello,<b>{session?.user?.name}</b></h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <img className="w-6 g-6" src={session?.user?.image} />
+          <span className="py-1 px-2">{session?.user?.name}</span>
+        </div>
+      </div>
+    </Layout>
+  );
+}
---
+++ pages/products.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from './components/Layout'
+
+const Products = () => {
+  return (
+<Layout>Product Page is here</Layout>
+)
+}
+
+export default Products
