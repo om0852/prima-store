4ca8a483ea7232efdaf0b22ef1ab7a307b3c7997
om salunke <omsaunke343@gmail.com>
2024-08-11 08:59:44 +0530

    chanege in callback url

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="w-full bg-gray-800 items-center text-white h-20 flex flex-row justify-between px-8">
       <h1 className="text-white flex">PrimaStore</h1>
       <ul className="flex flex-row justify-evenly gap-6 list-none">
-        <li className="cursor-pointer" onClick={() => signOut({callbackUrl:"http://localhost:3000"})}>Logout</li>
+        <li className="cursor-pointer" onClick={() => signOut()}>Logout</li>
       </ul>
     </div>
   );
